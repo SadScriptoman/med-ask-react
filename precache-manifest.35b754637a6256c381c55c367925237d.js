7567e8bbf4ec171957da925406f484d7b9d81d83
Nikita <mr.sokolov99@gmail.com>
2020-06-19 16:34:22 +0400

    Updates

---
+++ precache-manifest.35b754637a6256c381c55c367925237d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979f489dcfbf758d0e381ef69564ecb2",
+    "url": "/med-ask-react/index.html"
+  },
+  {
+    "revision": "5eb52dc7cf7ac1445667",
+    "url": "/med-ask-react/static/css/main.5f44ce0a.chunk.css"
+  },
+  {
+    "revision": "56c4f76e77edb2950b4a",
+    "url": "/med-ask-react/static/js/2.eb700878.chunk.js"
+  },
+  {
+    "revision": "5eb52dc7cf7ac1445667",
+    "url": "/med-ask-react/static/js/main.ae940b06.chunk.js"
+  },
+  {
+    "revision": "5f40e289925d3d515ebd",
+    "url": "/med-ask-react/static/js/runtime-main.5fbfe1ae.js"
+  }
+]);
